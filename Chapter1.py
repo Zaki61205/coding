15124640c788d3a412419422da42c0220f466ca0
Zaki61205 <zaki.gadhwalla@icloud.com>
2025-10-23 21:16:23 +0200

    nothing deleted the previus testing programm

--- Chapter1.py
+++
@@ -1,8 +0,0 @@
-print("Hello,Zaki!")
-print("Welcome to Chapter 1 of Coding!")
-print("Let's get started with some basic Python programming.")
-print("This chapter will cover variables, data types, and simple operations.")
-print("By the end of this chapter, you'll be able to write simple Python scripts.")
-print("Happy coding!")
-
-# This is a simple Python script for Chapter 1
